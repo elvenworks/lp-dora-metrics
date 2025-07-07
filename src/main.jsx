4dab5bd9602b7fbae3a072a8119a23be3524366d
Lucas Montenegro <lucasmontenegro475@gmail.com>
2025-07-07 19:32:13 -0300

    feat: add google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./lib/i18next.js";
 import App from "./App.jsx";
+import Analytics from "./components/common/Analytics";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
