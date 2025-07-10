4f3e63db14d623340d2a4bde4b9ab71bbd2eddc1
Fabricio Lopes <fabriciolopesvital@gmail.com>
2025-07-10 14:31:04 -0300

    fix GA

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./lib/i18next.js";
 import App from "./App.jsx";
-import Analytics from "./components/common/Analytics";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Analytics />
     <App />
   </StrictMode>
 );
